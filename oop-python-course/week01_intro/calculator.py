c27bbc926885ecc6f07223707947898fab572761
monsaluldshannen <monsaludshannen@gmail.com>
2026-04-10 12:32:50 +0800

    Add files via upload

---
+++ oop-python-course/week01_intro/calculator.py
@@ -0,0 +1,7 @@
+num1_shnn = float(input("Enter first number: "))
+num2_shnn = float(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
